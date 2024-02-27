4c8be2db565f72ae4b816715fb6008ca2a951253
elan <elandramaulana81@gmail.com>
2024-02-27 20:30:20 +0700

    checkout view

---
+++ resources/views/frontoffice/checkout/invoice_pdf.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice</title>
+    <!-- Include CSS styling if needed -->
+</head>
+<body>
+    <!-- Isi PDF sesuai dengan struktur tabel dan form yang diinginkan -->
+    @include('frontoffice.checkout.invoice_content')
+</body>
+</html>
