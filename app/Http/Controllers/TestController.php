13a4ff88a97a70ef8e841894c1dc90b9719592e1
Rizki Firdaus <firdausrizki17@gmail.com>
2024-04-24 20:09:08 +0700

    Progress : Feature Checkout History

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index(){
-        
+        $laund = new LaundryController();
+        $jumlah = $laund->getSumPrice(5);
+        print_r($jumlah);
     }
 }
