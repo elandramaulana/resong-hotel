251adc1c64f9c2e9a1f446d84f4570987ed3ec0d
elandramaulana <elandramaulana81@gmail.com>
2024-10-29 14:40:09 +0700

    prepare to user access

--- routes/web copy.php
+++ routes/web copy.php
@@ -57,38 +57,25 @@ require __DIR__ . '/auth.php';
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {});
-
-
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/checkin-normal', [CheckinController::class, 'index'])->name('checkin.normal');
-    Route::post('/checkin-normal', [CheckinController::class, 'store'])->name('checkin.normal.store');
-    Route::get('/checkin-normal-form/{id}', [CheckinController::class, 'form_normal'])->name('checkin.normal.form');
-    Route::get('/checkin-speedy', [CheckinController::class, 'speedy'])->name('checkin.speedy');
-    Route::post('/speedy-post', [CheckinController::class, 'speedy_post'])->name('checkin.speedy_post');
-});
-
-
-//ajax route
-Route::middleware('auth')->group(function () {
-    Route::get('/ajax-selectrooms', [RoomAjaxRequest::class, 'ajax_select_room'])->name('ajax.selectrooms');
-});
-
 
 
 
 // ============Route Owner==============//
 Route::middleware('auth')->group(function () {
     
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    //reservation
+    Route::get('/reservation-list', [BookingController::class, 'reservation_list'])->name('reservation.list');
+    Route::get('/booking-canceled/', [BookingController::class, 'booking_canceled'])->name('booking.canceled');
+    Route::get('/booking-no-showed/', [BookingController::class, 'no_showed'])->name('booking.no_showed');
+
+    //Guest
+    Route::get('/inhouse-guest', [InhouseController::class, 'index'])->name('inhouse.list');
+    Route::get('/checkout-history', [InhouseController::class, 'checkout_history'])->name('checked_out.list');
+    Route::get('/inhouse-table', [InhouseController::class, 'call_table'])->name('inhouse.table');
+    Route::get('/his_checkout-table', [InhouseController::class, 'his_checkout'])->name('his_checkout.table');
+    Route::get('/detail-inhouse-guest/{id}', [InhouseController::class, 'inhouse_detail'])->name('inhouse.details');
 });
 
 // ============End Route Owner==============//
@@ -111,6 +98,29 @@ Route::middleware('auth')->group(function () {
     
 });
 // ============End Route Owner==============//
+
+
+Route::middleware('auth')->group(function () {
+   
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/checkin-normal', [CheckinController::class, 'index'])->name('checkin.normal');
+    Route::post('/checkin-normal', [CheckinController::class, 'store'])->name('checkin.normal.store');
+    Route::get('/checkin-normal-form/{id}', [CheckinController::class, 'form_normal'])->name('checkin.normal.form');
+    Route::get('/checkin-speedy', [CheckinController::class, 'speedy'])->name('checkin.speedy');
+    Route::post('/speedy-post', [CheckinController::class, 'speedy_post'])->name('checkin.speedy_post');
+});
+
+
+//ajax route
+Route::middleware('auth')->group(function () {
+    Route::get('/ajax-selectrooms', [RoomAjaxRequest::class, 'ajax_select_room'])->name('ajax.selectrooms');
+});
+
 
 
 
@@ -159,27 +169,12 @@ Route::middleware('auth')->group(function () {
 // route generate pdf in invoice view
 Route::middleware('auth')->group(function () {
 
-
-    Route::view('/reservation-list', 'frontoffice/reservation/reservation_list')->name('reservation_list');
-    Route::get('/reservation-list', [BookingController::class, 'reservation_list'])->name('reservation.list');
-    Route::get('/booking-canceled/', [BookingController::class, 'booking_canceled'])->name('booking.canceled');
-    Route::get('/booking-no-showed/', [BookingController::class, 'no_showed'])->name('booking.no_showed');
-    Route::view('/edit-reservation', 'frontoffice/reservation/edit_reservation')->name('edit_reservation');
-
-
-    Route::view('/noshow-reservation-list', 'frontoffice/reservation/noshow_reservation_list')->name('noshow_reservation_list');
-
     // Guest View
-    Route::get('/inhouse-guest', [InhouseController::class, 'index'])->name('inhouse.list');
-    Route::get('/checkout-history', [InhouseController::class, 'checkout_history'])->name('checked_out.list');
-    Route::get('/inhouse-table', [InhouseController::class, 'call_table'])->name('inhouse.table');
-    Route::get('/his_checkout-table', [InhouseController::class, 'his_checkout'])->name('his_checkout.table');
+    
     Route::get('/inhouse-addextrabed', [InhouseController::class, 'add_extrabed'])->name('inhouse.add_extrabed');
     Route::post('/inhouse-postaddons', [InhouseController::class, 'add_addons'])->name('inhouse.postaddons');
     Route::get('/inhouse-deladdons', [InhouseController::class, 'del_addons'])->name('inhouse.del_addons');
-    Route::get('/detail-inhouse-guest/{id}', [InhouseController::class, 'inhouse_detail'])->name('inhouse.details');
-    Route::view('/detail-guest', 'frontoffice/guest/detail_guest')->name('detail_guest');
-    Route::view('/guest-database', 'frontoffice/guest/guest_database')->name('guest_database');
+    
 
     Route::get('/guest-autocomplete', [AutocompleteController::class, 'guests'])->name('autocomplete.guests');
     Route::get('/guest-speedy', [AutocompleteController::class, 'speedy'])->name('autocomplete.speedy');
