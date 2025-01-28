fbea30d3eac293c7ced0b33e6b5baf64d2777f1d
Elandra Maulana <elandramaulana81@gmail.com>
2025-01-28 15:58:20 +0700

    monthly transaction with filter

--- app/Http/Controllers/MonthlyReportController.php
+++ app/Http/Controllers/MonthlyReportController.php
@@ -1,13 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\TransaksiReport;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class MonthlyReportController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('frontoffice.report.monthly_report');
+        $bulan = $request->input('bulan');
+
+        $query = TransaksiReport::query();
+
+        $bulanMapping = [
+            'Januari' => 1, 'Februari' => 2, 'Maret' => 3, 'April' => 4,
+            'Mei' => 5, 'Juni' => 6, 'Juli' => 7, 'Agustus' => 8,
+            'September' => 9, 'Oktober' => 10, 'November' => 11, 'Desember' => 12
+        ];
+
+        if ($bulan && isset($bulanMapping[$bulan])) {
+            $query->whereMonth('created_at', $bulanMapping[$bulan]);
+        }
+
+        $transaksi = $query->orderBy('created_at', 'asc')->get();
+
+        // Hitung total transaksi
+        $totalTransaksi = $transaksi->sum('besar_transaksi');
+
+        return view('frontoffice.report.monthly_report', compact('transaksi', 'bulan', 'totalTransaksi'));
     }
 }
