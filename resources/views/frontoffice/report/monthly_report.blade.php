953c80702df6a9f48be8895abfbbd6fdb2942d37
Rizki Firdaus <firdausrizki17@gmail.com>
2025-02-20 20:12:36 +0700

    alter table transaction_reports & link href daily report

--- resources/views/frontoffice/report/monthly_report.blade.php
+++ resources/views/frontoffice/report/monthly_report.blade.php
@@ -62,7 +62,7 @@
                                         <div>
                                             <button style="margin-right: 10px" type="submit"
                                                 class="btn btn-warning btn-sm mt-2">
-                                                <a style="color: black" href="#"> <i class="fas fa-eye"></i></a>
+                                                <a style="color: black" href="{{route('weekly.report', ['date' => $data['date'] ])}}"> <i class="fas fa-eye"></i></a>
                                             </button>
                                         </div>
                                     </td>
